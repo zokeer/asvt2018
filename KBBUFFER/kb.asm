c1dfde669c6f63aff00afaa7a35524a748187b71
zokeer <zokeerxd@gmail.com>
2018-04-03 21:43:21 +0500

    Fix

--- KBBUFFER/kb.asm
+++ KBBUFFER/kb.asm
@@ -3,12 +3,16 @@
     org 100h
 start:
 	jmp work
-	buffer db 6, 0,0,0,0,0,0, 0, 24h
+	buffer db 6, 0,6 dup (0dh), 24h
 	InputRequest db "Input up to 5 characters: ", 24h
 	OutputReadyString db "You've gave me: ", 24h
 work:
 	mov ah, 09h
 	lea dx, InputRequest
+	int 21h
+
+	mov ah, 02h
+	mov dl, 10
 	int 21h
 
 	mov ah, 0Ah
@@ -21,6 +25,10 @@ work:
 
 	mov ah, 09h
 	lea dx, OutputReadyString
+	int 21h
+
+	mov ah, 02h
+	mov dl, 10
 	int 21h
 
 	mov ah, 09h
